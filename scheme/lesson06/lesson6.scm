9f073e5b092ad8dbcb21393871a2855fe86a65c6
Lassi Kortela <lassi@lassi.io>
2019-10-15 19:18:56 +0300

    Brush up the Scheme source code (#4)
    * Rename Scheme archives with proper .tar extension
    
    * Replace Scheme archive files with their contents
    
    * Fix whitespace and use UTF-8 in Scheme source
    
    * Replace Unicode symbols with ASCII in Scheme source

--- scheme/lesson06/lesson6.scm
+++ scheme/lesson06/lesson6.scm
@@ -1,54 +1,40 @@
-;; PLT Scheme version of NeHe's tutorial #1
-;;
-;; Written and tested with WinXP Pro + DrScheme 205 + sgl
-;; if you test this under any *nix (including FreeBSD or
-;; any other non-linix *nix) please write me at bcj1980 at
-;; sbcglobal dot net and tell me about the results.
-;;
-;; Disclaimer:
-;; I am not only a non "guru" programmer, I'm
-;; new to scheme, OpenGL, and MrEd.  Thankfully, I have
-;; many years experience with 3d algorithims, so it comes
-;; fairly easily to me, but don't be intimitaded by my
-;; coding style.  This tutorial was ported under the
-;; assumption that you know C, and are following along
-;; with NeHe's commented code and not just relying on
-;; mine.  Also, I don't attempt to explain the majority
-;; of the scheme-centric code and I assume you are
-;; fairly comfortable with scheme by now if you are
-;; trying out OpenGL bindings.  Also, I explicitly
-;; disclaim any responsibility for firey explosions/
-;; monitor death/melting video cards, or anything else
-;; unpleaseant that may happen to you, your hardware,
-;; or your software as a result of you using my code.
-
-(require (lib "gl.ss" "sgl")
-         (lib "gl-vectors.ss" "sgl")
-         (lib "math.ss" "mzlib"))
+(require
+ (lib "gl.ss" "sgl")
+ (lib "gl-vectors.ss" "sgl"))
 
 
+;; Structure to hold a loaded texture image
+(define-struct texture (width height data))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Helper functions
-
-;; This is a kludge for lack of GLU support in SGL.
-;; I choose to keep the name in case SGL gets GLU
-;; support later.  You probably shouldn't try to
-;; understand this yet unless you're masochistic or
-;; want an explosive brain hemorrhage. There is no
-;; error checking, so be careful.
-
-(define (gluPerspective fovy aspect znear zfar)
-  (let ((f (/ 1 (tan (/ (* fovy (/ pi 180)) 2))))
-        (g (- znear zfar)))
-    (glMultMatrixd
-     (vector->gl-double-vector
-      (vector
-       (/ f aspect) 0 0 0
-       0 f 0 0
-       0 0 (/ (+ znear zfar) g) -1
-       0 0 (/ (* 2 znear zfar) g) 0)))))
-
+;; Load a texture (DrScheme specific)
+ (define load-texture
+    (lambda (file)
+      (let* (
+             (bmp  (make-object bitmap% file 'unknown #f))
+             (dc (instantiate bitmap-dc% (bmp)))
+             (width (send bmp get-width))
+             (height (send bmp get-height))
+             (pixels (* width height))
+             (vec (make-vector (* pixels 3)))
+             (data (make-string (* pixels 4)))
+             (i 0)
+             )
+        (send dc get-argb-pixels 0 0 width height data)
+        (letrec
+            ([loop
+              (lambda ()
+                (if (< i pixels)
+                    (begin
+                      (vector-set! vec (* i  3)
+                                   (char->integer (string-ref data (+ (* i 4) 1))))
+                      (vector-set! vec (+ (* i 3) 1)
+                                   (char->integer (string-ref data (+ (* i 4) 2))))
+                      (vector-set! vec (+ (* i 3) 2)
+                                   (char->integer (string-ref data (+ (* i 4) 3))))
+                      (set! i (+ i 1))
+                      (loop))))])
+          (loop))
+        (make-texture width height (vector->gl-ubyte-vector vec)))))
 
 ;; This function returns a counter that increments
 ;; each time it is called. It takes three
@@ -76,114 +62,101 @@
       (set! value (+ value step))
       (loop))))
 
+(define xrot (counter-generator 0 0.2 0 360))
+(define yrot (counter-generator 0 0.3 0 360))
+(define zrot (counter-generator 0 0.4 0 360))
 
+;; A vector to hold texture numbers
+(define *texture* (vector->gl-uint-vector (make-vector 1)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; This is our main drawing function
+;; Our initialization function
+(define gl-init
+  (lambda ()
+    (let ((texture (load-texture "NeHe.bmp")))
+      (glGenTextures 1 *texture*)
+      (glBindTexture GL_TEXTURE_2D (gl-uint-vector-ref *texture* 0))
+      (glTexImage2D GL_TEXTURE_2D 0 3 (texture-width texture) (texture-height texture)
+                    0 GL_RGB GL_UNSIGNED_BYTE (texture-data texture))
+      (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_LINEAR)
+      (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_LINEAR))
 
-
-;; Our rotation variables
-(define trirot (counter-generator 0 2 0 360))
-(define quadrot (counter-generator 0 -1.5 0 360))
-
+      (glEnable GL_TEXTURE_2D)
+      (glShadeModel GL_SMOOTH)
+      (glClearColor 0.0 0.0 0.0 0.5)
+      (glClearDepth 1.0)
+      (glEnable GL_DEPTH_TEST)
+      (glDepthFunc GL_LEQUAL)
+      (glHint GL_PERSPECTIVE_CORRECTION_HINT GL_NICEST)))
 
 ;; Our main function that does the drawing
 (define (gl-draw)
   (glClear (+ GL_COLOR_BUFFER_BIT GL_DEPTH_BUFFER_BIT))
 
-  ;;----------------------------------------------;;
-  ;; Here we just change last tutorial to draw in 3d
-  ;; , change the second glTranslate, add an extra
-  ;; glLoadIdentity, and twiddle the rotation.
-
   (glLoadIdentity)
-  (glTranslatef -1.5 0 -6)
-  (glRotatef (trirot) 0 1 0)
-
-  (glBegin GL_TRIANGLES)
-
-  (glColor3f 1 0 0)
-  (glVertex3f 0 1 0)
-  (glColor3f 0 1 0)
-  (glVertex3f -1 -1 1)
-  (glColor3f 0 0 1)
-  (glVertex3f 1 -1 1)
-
-  (glColor3f 1 0 0)
-  (glVertex3f 0 1 0)
-  (glColor3f 0 0 1)
-  (glVertex3f 1 -1 1)
-  (glColor3f 0 1 0)
-  (glVertex3f 1 -1 -1)
-
-  (glColor3f 1 0 0)
-  (glVertex3f 0 1 0)
-  (glColor3f 0 1 0)
-  (glVertex3f 1 -1 -1)
-  (glColor3f 0 0 1)
-  (glVertex3f -1 -1 -1)
-
-  (glColor3f 1 0 0)
-  (glVertex3f 0 1 0)
-  (glColor3f 0 0 1)
-  (glVertex3f -1 -1 -1)
-  (glColor3f 0 1 0)
-  (glVertex3f -1 -1 1)
-
-  (glEnd)
-
-  (glLoadIdentity)
-  (glTranslatef 1.5 0 -7)
-  (glRotatef (quadrot) 1 1 1)
+  (glTranslatef 0 0 -5)
+  (glRotatef (xrot) 1 0 0)
+  (glRotatef (yrot) 0 1 0)
+  (glRotatef (zrot) 0 0 1)
+  (glBindTexture GL_TEXTURE_2D (gl-uint-vector-ref *texture* 0))
 
   (glBegin GL_QUADS)
-
-  (glColor3f 0 1 0)
-  (glVertex3f 1 1 -1)
-  (glVertex3f -1 1 -1)
-  (glVertex3f -1 1 1)
-  (glVertex3f 1 1 1)
-
-  (glColor3f 1 0.5 0)
-  (glVertex3f 1 -1 1)
+  ; front
+  (glTexCoord2f 0 0)
   (glVertex3f -1 -1 1)
-  (glVertex3f -1 -1 -1)
-  (glVertex3f 1 -1 -1)
-
-  (glColor3f 1 0 0)
-  (glVertex3f 1 1 1)
-  (glVertex3f -1 1 1)
-  (glVertex3f -1 -1 1)
+  (glTexCoord2f 0 1)
   (glVertex3f 1 -1 1)
-
-  (glColor3f 1 1 0)
-  (glVertex3f 1 -1 -1)
-  (glVertex3f -1 -1 -1)
-  (glVertex3f -1 1 -1)
-  (glVertex3f 1 1 -1)
-
-  (glColor3f 0 0 1)
-  (glVertex3f -1 1 1)
-  (glVertex3f -1 1 -1)
-  (glVertex3f -1 -1 -1)
-  (glVertex3f -1 -1 1)
-
-  (glColor3f 1 0 1)
-  (glVertex3f 1 1 -1)
+  (glTexCoord2f 1 1)
   (glVertex3f 1 1 1)
-  (glVertex3f 1 -1 1)
+  (glTexCoord2f 1 0)
+  (glVertex3f -1 1 1)
+  ; back
+  (glTexCoord2f 1 0)
+  (glVertex3f -1 -1 -1)
+  (glTexCoord2f 1 1)
   (glVertex3f 1 -1 -1)
+  (glTexCoord2f 0 1)
+  (glVertex3f 1 1 -1)
+  (glTexCoord2f 0 0)
+  (glVertex3f -1 1 -1)
+  ; top
+  (glTexCoord2f 0 1)
+  (glVertex3f -1 1 -1)
+  (glTexCoord2f 0 0)
+  (glVertex3f 1 1 -1)
+  (glTexCoord2f 1 0)
+  (glVertex3f 1 1 1)
+  (glTexCoord2f 1 1)
+  (glVertex3f -1 1 1)
+  ; bottom
+   (glTexCoord2f 1 1)
+  (glVertex3f -1 -1 -1)
+  (glTexCoord2f 0 1)
+  (glVertex3f -1 -1 1)
+  (glTexCoord2f 0 0)
+  (glVertex3f 1 -1 1)
+  (glTexCoord2f 1 0)
+  (glVertex3f 1 -1 -1)
+  ; right
+  (glTexCoord2f 1 0)
+  (glVertex3f 1 -1 -1)
+  (glTexCoord2f 1 1)
+  (glVertex3f 1 -1 1)
+  (glTexCoord2f 0 1)
+  (glVertex3f 1 1 1)
+  (glTexCoord2f 0 0)
+  (glVertex3f 1 1 -1)
+  ;left
+   (glTexCoord2f 0 0)
+  (glVertex3f -1 -1 -1)
+  (glTexCoord2f 1 0)
+  (glVertex3f -1 1 -1)
+  (glTexCoord2f 1 1)
+  (glVertex3f -1 1 1)
+  (glTexCoord2f 0 1)
+  (glVertex3f -1 -1 1)
 
   (glEnd)
-
   (glFlush))
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Drawing, resizing, key handling
-;; and initilization functions
-
 
 ;; This function calls gl-init once and then sets itself to call gl-draw
 (define (gl-thunk)
@@ -213,17 +186,7 @@
   (glMatrixMode GL_MODELVIEW)
   (glLoadIdentity))
 
-
-;; A function to initialize the OpenGL state
-(define (gl-init)
-  (glShadeModel GL_SMOOTH)
-  (glClearColor 0 0 0 0.5)
-  (glClearDepth 1)
-  (glEnable GL_DEPTH_TEST)
-  (glDepthFunc GL_LEQUAL)
-  (glHint GL_PERSPECTIVE_CORRECTION_HINT GL_NICEST))
-
-;; The key handeling function
+; The key handeling function
 (define (gl-handlekey key)
   (let ((k (send key get-key-code)))
         (cond ((eq? k 'escape) (exit))
@@ -239,14 +202,10 @@
                           (= (send frame get-height) y))
                      (send frame maximize #f)))))))
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Windowing setup
-
 ;; Make a 640 x 480 frame
 (define frame
   (instantiate frame% ()
-    (label "NeHe's OpenGL Tutorial #5 - ported by Bandit Cat")
+    (label "NeHe's OpenGL Tutorial #6 - ported by BBurns")
     (width 640)
     (height 480)))
 
